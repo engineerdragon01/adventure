0111f2cd7403dd64b64186c42134b457fbab156b
Adam <dragonboy174@gmail.com>
2019-07-22 16:59:55 -0700

    hey

--- main.py
+++ main.py
@@ -14,8 +14,22 @@ class MainHandler(webapp2.RequestHandler):
         template = env.get_template("templates/start.html")
         self.response.write(template.render())
 
+class RunHandler(webapp2.RequestHandler):
+    def get(self):
+        template = env.get_template("templates/run.html")
+        self.response.write(template.render())
+
+class JumpHandler(webapp2.RequestHandler):
+    def get(self):
+        template = env.get_template("templates/jump.html")
+        self.response.write(template.render())
+
+
+
 
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
+    ('/run', RunHandler),
+    ('/jump', JumpHandler),
 ], debug = True)
